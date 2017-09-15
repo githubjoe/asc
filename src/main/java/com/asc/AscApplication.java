8798639872cda5d4a9d0b70721b1014872934779
lrm <liuruming163@163.com>
2017-09-15 17:33:38 +0800

    asc init

---
+++ src/main/java/com/asc/AscApplication.java
@@ -0,0 +1,12 @@
+package com.asc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscApplication.class, args);
+	}
+}
